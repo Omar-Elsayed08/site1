9d30e166c2a191deeb7c3d7f1a423693cebbb141
Omar-Elsayed08 <141748556+Omar-Elsayed08@users.noreply.github.com>
2024-09-09 08:09:35 -0500

    new

---
+++ datatypes.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+    //String
+    $name= "Coding is fun";
+
+    //Integer
+    $name= 10;
+
+    //Float
+    $name= 10.5434;
+
+    //Boolean
+    
+
+    //Array
+    $names= array("Coach Clark","Coach Maxwell","Coach B");
+    echo $names['2'];
+?>
+
+
+</body>
+</html>
